33295f741327d90e6e970cda104e558b9668a1de
Maciek Wojnar <mwojnar83@gmail.com>
2021-05-02 23:18:39 +0200

    first stage of refactoring js

--- curency-calculator/script.js
+++ curency-calculator/script.js
@@ -1,48 +1,45 @@
-const amountPlnElement = document.querySelector(".js-amountPln");
-const rateEurElement = document.querySelector(".js-rateEur");
-const rateUsdElement = document.querySelector(".js-rateUsd");
-const rateGbpElement = document.querySelector(".js-rateGbp");
-const rateChfElement = document.querySelector(".js-rateChf");
-const formElement = document.querySelector(".js-form");
-const exchangeElement = document.querySelector(".js-exchange");
-const selectedCurrencyElement = document.querySelector(".js-selectedCurrency");
+{
+  const formElement = document.querySelector(".js-form");
+  formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const amountPlnElement = document.querySelector(".js-amountPln");
+    const rateEurElement = document.querySelector(".js-rateEur");
+    const rateUsdElement = document.querySelector(".js-rateUsd");
+    const rateGbpElement = document.querySelector(".js-rateGbp");
+    const exchangeElement = document.querySelector(".js-exchange");
+    const selectedCurrencyElement = document.querySelector(".js-selectedCurrency");
 
-const realizations = document.querySelector(".js-exchangeRate");
-const showContent = document.querySelector(".js-showContentButton");
-const textButton = document.querySelector(".js-textButton");
+    const amountPln = amountPlnElement.value;
+    const rateEur = rateEurElement.value;
+    const rateUsd = rateUsdElement.value;
+    const rateGbp = rateGbpElement.value;
+    const selectedCurrency = selectedCurrencyElement.value;
+    
 
-formElement.addEventListener("submit", (event) => {
-  event.preventDefault();
+    switch (selectedCurrency) {
+      case "EUR":
+        exchange = amountPln / rateEur;
+        break;
+      case "USD":
+        exchange = amountPln / rateUsd;
+        break;
+      case "GBP":
+        exchange = amountPln / rateGbp;
+        break;
+    }
 
-  const amountPln = amountPlnElement.value;
-  const rateEur = rateEurElement.value;
-  const rateUsd = rateUsdElement.value;
-  const rateGbp = rateGbpElement.value;
-  const selectedCurrency = selectedCurrencyElement.value;
+    exchangeElement.innerText = `${exchange.toFixed(2)} ${selectedCurrency}`;
 
+  });
 
-  switch (selectedCurrency) {
-    case "EUR":
-      exchange = amountPln / rateEur;
-      break;
-    case "USD":
-      exchange = amountPln / rateUsd;
-      break;
-    case "GBP":
-      exchange = amountPln / rateGbp;
-      break;
-    default:
-      console.log("Woops. Something went wrong!");
-      break;
-  }
-  
-  exchangeElement.innerText = `${exchange.toFixed(2)} ${selectedCurrency}`;
-  
-});
+}
 
-
-showContent.addEventListener("click", () => {
-  realizations.classList.toggle("hidden");
-
-  textButton.innerText = realizations.classList.contains("hidden") ? "Pokaż" : "Ukryj";
-});
+{
+  const showContent = document.querySelector(".js-showContentButton");
+  showContent.addEventListener("click", () => {
+    const textButton = document.querySelector(".js-textButton");
+    const realizations = document.querySelector(".js-exchangeRate");
+    realizations.classList.toggle("hidden");
+    textButton.innerText = realizations.classList.contains("hidden") ? "Pokaż" : "Ukryj";
+  });
+}
