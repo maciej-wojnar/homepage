ed25f899d5ef176e6c43e080c0e69e67ebd3b3ec
Maciek Wojnar <mwojnar83@gmail.com>
2021-05-03 23:02:29 +0200

    Initial commit

---
+++ to-do-list/script/script.js
@@ -0,0 +1,34 @@
+{
+    const tasks = [
+        {
+            content: "wstać",
+            done: false,
+        },
+        {
+            content: "zjeść śniadanie",
+            done: true,
+
+        },
+
+    ];
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+              <li>
+              ${task.content}
+              </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const init = () => {
+        render();
+    };
+
+    init();
+}
