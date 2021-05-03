9752c2bfa374f241520b4f54af32c28ae5af30df
Maciek Wojnar <mwojnar83@gmail.com>
2021-05-03 02:15:55 +0200

    icon and image added

--- curency-calculator/script.js
+++ curency-calculator/script.js
@@ -1,45 +1,46 @@
 {
+
   const formElement = document.querySelector(".js-form");
   formElement.addEventListener("submit", (event) => {
     event.preventDefault();
-    const amountPlnElement = document.querySelector(".js-amountPln");
+
     const rateEurElement = document.querySelector(".js-rateEur");
     const rateUsdElement = document.querySelector(".js-rateUsd");
     const rateGbpElement = document.querySelector(".js-rateGbp");
     const exchangeElement = document.querySelector(".js-exchange");
+
+    const amountPlnElement = document.querySelector(".js-amountPln");
     const selectedCurrencyElement = document.querySelector(".js-selectedCurrency");
 
-    const amountPln = amountPlnElement.value;
-    const rateEur = rateEurElement.value;
-    const rateUsd = rateUsdElement.value;
-    const rateGbp = rateGbpElement.value;
-    const selectedCurrency = selectedCurrencyElement.value;
-    
-
-    switch (selectedCurrency) {
-      case "EUR":
-        exchange = amountPln / rateEur;
-        break;
-      case "USD":
-        exchange = amountPln / rateUsd;
-        break;
-      case "GBP":
-        exchange = amountPln / rateGbp;
-        break;
+    const calculateExchange = (amountPln, selectedCurrency) => {
+      const rateEur = rateEurElement.value;
+      const rateUsd = rateUsdElement.value;
+      const rateGbp = rateGbpElement.value;
+      switch (selectedCurrency) {
+        case "EUR":
+          return amountPln / rateEur;
+        case "USD":
+          return amountPln / rateUsd;
+        case "GBP":
+          return amountPln / rateGbp;
+      }
     }
+    const amountPln = amountPlnElement.value;
+    const selectedCurrency = selectedCurrencyElement.value;
 
+    const exchange = calculateExchange(amountPln, selectedCurrency)
     exchangeElement.innerText = `${exchange.toFixed(2)} ${selectedCurrency}`;
 
   });
 
-}
-
-{
-  const showContent = document.querySelector(".js-showContentButton");
-  showContent.addEventListener("click", () => {
-    const textButton = document.querySelector(".js-textButton");
-    const realizations = document.querySelector(".js-exchangeRate");
+  const showRateTable = () => {
     realizations.classList.toggle("hidden");
+
     textButton.innerText = realizations.classList.contains("hidden") ? "Pokaż" : "Ukryj";
-  });
+  }
+  const showContent = document.querySelector(".js-showContentButton");
+  const realizations = document.querySelector(".js-exchangeRate");
+  const textButton = document.querySelector(".js-textButton");
+  showContent.addEventListener("click", showRateTable);
+
 }
